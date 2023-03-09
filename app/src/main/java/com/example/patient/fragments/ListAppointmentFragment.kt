8376b66b3c2741bab6e3c5939ee9d2f48ee2d7b7
Lyan-bit <72354386+Lyan-bit@users.noreply.github.com>
2023-03-09 00:39:09 +0000

    Update ListAppointmentFragment.kt

--- app/src/main/java/com/example/patient/fragments/ListAppointmentFragment.kt
+++ app/src/main/java/com/example/patient/fragments/ListAppointmentFragment.kt
@@ -9,7 +9,6 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.patient.viewmodel.PatientViewModel
 import com.example.patient.R
 import com.example.patient.adapter.AppointmentRecyclerViewAdapter
 import com.example.patient.model.AppointmentVO
