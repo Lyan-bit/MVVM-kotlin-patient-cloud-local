9e91a3a8a32ce3de5c0341b2e77c039974747bf8
Lyan-bit <72354386+Lyan-bit@users.noreply.github.com>
2023-06-11 20:49:31 +0100

    Update DeletePatientFragment.kt

--- app/src/main/java/com/example/patient/fragments/DeletePatientFragment.kt
+++ app/src/main/java/com/example/patient/fragments/DeletePatientFragment.kt
@@ -15,23 +15,31 @@ import com.example.patient.model.PatientBean
 import java.lang.Exception
 import java.util.ArrayList
 
-class DeletePatientFragment : Fragment(), View.OnClickListener, AdapterView.OnItemSelectedListener {
+
+class EditPatientFragment : Fragment(), View.OnClickListener, AdapterView.OnItemSelectedListener {
 	private lateinit var root: View
 	private lateinit var myContext: Context
 	private lateinit var model: PatientViewModel
 	
 	private lateinit var patientBean: PatientBean
 	
-	private lateinit var deletePatientSpinner: Spinner
+	private lateinit var editPatientSpinner: Spinner
 	private var allPatientpatientIds: List<String> = ArrayList()
+
+	private lateinit var searchPatientButton : Button	
+	private lateinit var editPatientButton : Button
+	private lateinit var cancelPatientButton : Button	
+
 	private lateinit var patientIdTextField: EditText
 	private var patientIdData = ""
-	private lateinit var deletePatientButton : Button
-	private lateinit var cancelPatientButton : Button	
-	
+	private lateinit var nameTextField: EditText
+	private var nameData = ""
+	private lateinit var appointmentIdTextField: EditText
+	private var appointmentIdData = ""
+
     companion object {
-        fun newInstance(c: Context): DeletePatientFragment {
-            val fragment = DeletePatientFragment()
+        fun newInstance(c: Context): EditPatientFragment {
+            val fragment = EditPatientFragment()
             val args = Bundle()
             fragment.arguments = args
             fragment.myContext = c
@@ -40,35 +48,38 @@ class DeletePatientFragment : Fragment(), View.OnClickListener, AdapterView.OnIt
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-		root = inflater.inflate(R.layout.deletepatient_layout, container, false)
-	    return root
-	}
-	
-	override fun onResume() {
-		super.onResume()
+		root = inflater.inflate(R.layout.editpatient_layout, container, false)
 		model = PatientViewModel.getInstance(myContext)
-		patientBean = PatientBean(myContext)
 
-		patientIdTextField = root.findViewById(R.id.crudPatientpatientIdField)
-		deletePatientSpinner = root.findViewById(R.id.crudPatientSpinner)
+		editPatientSpinner = root.findViewById(R.id.crudPatientSpinner)
 		Log.i("model",model.listPatient().toString())
 		Log.i("model",model.allPatientPatientIds().toString())
 
 		allPatientpatientIds = model.allPatientPatientIds()
-		val deletePatientAdapter =
+		val editPatientAdapter =
 		ArrayAdapter(myContext, android.R.layout.simple_spinner_item, allPatientpatientIds)
-		deletePatientAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-		deletePatientSpinner.adapter = deletePatientAdapter
-		deletePatientSpinner.onItemSelectedListener = this
+		editPatientAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+		editPatientSpinner.adapter = editPatientAdapter
 
-		deletePatientButton = root.findViewById(R.id.crudPatientOK)
-		deletePatientButton.setOnClickListener(this)
+		editPatientSpinner.onItemSelectedListener = this
+		
+		patientBean = PatientBean(myContext)
+		searchPatientButton = root.findViewById(R.id.crudPatientSearch)
+		searchPatientButton.setOnClickListener(this)
+		editPatientButton = root.findViewById(R.id.crudPatientOK)
+		editPatientButton.setOnClickListener(this)
 		cancelPatientButton = root.findViewById(R.id.crudPatientCancel)
 		cancelPatientButton.setOnClickListener(this)
+
+		patientIdTextField = root.findViewById(R.id.crudPatientpatientIdField)
+		nameTextField = root.findViewById(R.id.crudPatientnameField)
+		appointmentIdTextField = root.findViewById(R.id.crudPatientappointmentIdField)
+
+		return root
 	}
 	
 	override fun onItemSelected(parent: AdapterView<*>, v: View?, position: Int, id: Long) {
-		if (parent === deletePatientSpinner) {
+		if (parent === editPatientSpinner) {
 			patientIdTextField.setText(allPatientpatientIds[position])
 		}
 	}
@@ -86,30 +97,57 @@ class DeletePatientFragment : Fragment(), View.OnClickListener, AdapterView.OnIt
 		}
 
 	when (v.id) {
+		R.id.crudPatientSearch -> {
+			crudPatientSearch()
+		}			
 		R.id.crudPatientOK -> {
-			deletePatientOK()
+			crudPatientOK()
 		}
 		R.id.crudPatientCancel -> {
-			deletePatientCancel()
+			crudPatientCancel()
 		}
 	  }
     }
-
-	private fun deletePatientOK() {
+    
+	private fun crudPatientSearch() {
 		patientIdData = patientIdTextField.text.toString()
 		patientBean.setPatientId(patientIdData)
-		if (patientBean.isDeletePatientError(allPatientpatientIds)) {
+		
+		if (patientBean.isSearchPatientIdError(allPatientpatientIds)) {
 			Log.w(javaClass.name, patientBean.errors())
 			Toast.makeText(myContext, "Errors: " + patientBean.errors(), Toast.LENGTH_LONG).show()
 		} else {
-		patientBean.deletePatient()
-		Toast.makeText(myContext, "patient is deleted!", Toast.LENGTH_LONG).show()
+			val selectedItem = model.searchPatientById(patientIdData)
+				patientIdTextField.setText(selectedItem.patientId.toString())
+				nameTextField.setText(selectedItem.name.toString())
+				appointmentIdTextField.setText(selectedItem.appointmentId.toString())
+				Toast.makeText(myContext, "search Patient is done!", Toast.LENGTH_LONG).show()
 		}
 	}
 
-	private fun deletePatientCancel() {
+	private fun crudPatientOK() {
+		patientIdData = patientIdTextField.text.toString()
+			patientBean.setPatientId(patientIdData)
+		nameData = nameTextField.text.toString()
+			patientBean.setName(nameData)
+		appointmentIdData = appointmentIdTextField.text.toString()
+			patientBean.setAppointmentId(appointmentIdData)
+
+		if (patientBean.isEditPatientError(allPatientpatientIds)) {
+			Log.w(javaClass.name, patientBean.errors())
+			Toast.makeText(myContext, "Errors: " + patientBean.errors(), Toast.LENGTH_LONG).show()
+		} else {
+		patientBean.editPatient()
+		Toast.makeText(myContext, "Patient edited!", Toast.LENGTH_LONG).show()
+		}
+	}
+
+	private fun crudPatientCancel() {
 		patientBean.resetData()
 		patientIdTextField.setText("")
+		nameTextField.setText("")
+		appointmentIdTextField.setText("")
 	}
+	
 		 
 }
